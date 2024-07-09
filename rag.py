ed039f26a899339c5952fbbccad6c9db92bd36a0
DeepInTheSoul <110331026+DeepInTheSoul@users.noreply.github.com>
2024-07-09 22:33:03 +0800

    Update rag.py

--- rag.py
+++ rag.py
@@ -15,10 +15,6 @@ import codecs
 import csv
 config = dotenv_values(".env")
 
-
-# os.environ["QIANFAN_AK"] = "rYndCW8UyNrh7ZIxAmxG0w1X"
-# os.environ["QIANFAN_SK"] = "KovKWoaJeKYeIQwLgOUxFof5KI1ggTRq"
-# embeddings=QianfanEmbeddingsEndpoint(model='bge-large-zh')
 embeddings=QianfanEmbeddingsEndpoint(model='bge-large-zh',qianfan_ak='rYndCW8UyNrh7ZIxAmxG0w1X',qianfan_sk='KovKWoaJeKYeIQwLgOUxFof5KI1ggTRq')
 
 def rag_page():
